ba7feda229387f6aa2f927c0d2d590840529dae9
kendlbat <kendlbat@edu.htl-villach.at>
2023-04-18 22:27:30 +0200

    Remove index.js

--- root/beta/webconsole.js
+++ root/beta/webconsole.js
@@ -414,3 +414,18 @@ class WebConsole {
         this.#historypos = this.#history.length;
     }
 }
+
+/**
+ * @type {WebConsole}
+ */
+let mainconsole;
+
+document.addEventListener('DOMContentLoaded', () => {
+    mainconsole = new WebConsole(document.querySelector('#webconsole-container'));
+    mainconsole.loadHistoryFromLocalStorage("kendlportfolio");
+});
+
+window.addEventListener('beforeunload', (e) => {
+    mainconsole.saveHistoryToLocalStorage("kendlportfolio");
+});
+
