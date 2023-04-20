234ec86d7b4874da4ab28adcc9aec0b52af36278
kendlbat <kendlbat@edu.htl-villach.at>
2023-04-20 23:10:17 +0200

    Fix loading script

--- root/beta/webconsole.js
+++ root/beta/webconsole.js
@@ -753,12 +753,13 @@ class WebConsole {
  */
 let mainconsole;
 
-document.addEventListener('DOMContentLoaded', () => {
+function initMainWebconsole() {
     mainconsole = new WebConsole(document.querySelector('#webconsole-container'));
     mainconsole.loadHistoryFromLocalStorage("kendlportfolio");
-});
 
-window.addEventListener('beforeunload', (e) => {
-    mainconsole.saveHistoryToLocalStorage("kendlportfolio");
-});
+    window.addEventListener('beforeunload', (e) => {
+        mainconsole.saveHistoryToLocalStorage("kendlportfolio");
+    });    
+}
 
+globalThis.initMainWebconsole = initMainWebconsole;
