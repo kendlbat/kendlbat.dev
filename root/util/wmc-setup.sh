d3cd9158ec0947ce82446722f05b6589d45f0c55
Tobias Kendlbacher <kendlbat@edu.htl-villach.at>
2023-09-25 11:03:23 +0200

    VSCode font setup

--- root/util/wmc-setup.sh
+++ root/util/wmc-setup.sh
@@ -7,4 +7,10 @@ alias git-credential-manager='~/gcm/usr/local/bin/git-credential-manager'
 export GCM_CREDENTIAL_STORE=cache
 $HOME/gcm/usr/local/bin/git-credential-manager configure
 git clone https://gitlab.com/htl-villach/informatik/2023-4bhif/wmc/KENDLBACHER $HOME/wmc_kendlbat
+curl -o /tmp/JBMono.zip -L https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip
+unzip /tmp/JBMono.zip -d $HOME/JBMono
+mkdir -p $HOME/.local/share/fonts
+mv $HOME/JBMono/fonts/ttf/* $HOME/.local/share/fonts/
+rm $HOME/.config/Code/User/settings.json
+curl -o $HOME/.config/Code/User/settings.json -L https://kendlbat.dev/util/wmcsetup/settings.json
 code $HOME/wmc_kendlbat
