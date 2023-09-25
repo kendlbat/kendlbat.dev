4f20e99e912797df1564dbe75b2f2c5057da8655
Tobias Kendlbacher <kendlbat@edu.htl-villach.at>
2023-09-25 09:56:46 +0200

    Create wmc-setup.sh

---
+++ root/util/wmc-setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark && gsettings set org.gnome.desktop.interface gtk-color-scheme prefer-dark
+curl -o gcm.deb https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.3.2/gcm-linux_amd64.2.3.2.deb
+dpkg -x ~/gcm gcm.deb
+alias git-credential-manager='~/gcm/usr/local/bin/git-credential-manager'
+export GCM_CREDENTIAL_STORE=cache
+git clone https://gitlab.com/htl-villach/informatik/2023-4bhif/wmc/KENDLBACHER ~/wmc_kendlbat
