8c384453eb33694640d87e9ca48c075ce03d41b1
Tobias Kendlbacher <kendlbat@edu.htl-villach.at>
2023-10-11 11:43:03 +0200

    Update wmc-setup.sh

--- root/util/wmc-setup.sh
+++ root/util/wmc-setup.sh
@@ -4,6 +4,7 @@ gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark && gsettings se
 curl -o /tmp/gcm_install.deb -L https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.3.2/gcm-linux_amd64.2.3.2.deb
 dpkg -x /tmp/gcm_install.deb $HOME/gcm
 alias git-credential-manager='~/gcm/usr/local/bin/git-credential-manager'
+echo alias git-credential-manager='~/gcm/usr/local/bin/git-credential-manager' >> $HOME/.bashrc
 export GCM_CREDENTIAL_STORE=cache
 git config --global credential.credentialStore cache
 $HOME/gcm/usr/local/bin/git-credential-manager configure
