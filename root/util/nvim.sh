68626e5e95facc23e27ce050d72d8b865e7c34bb
Tobias Kendlbacher <kendlbat@edu.htl-villach.at>
2023-10-16 10:47:59 +0200

    Nvim wrong dir name

--- root/util/nvim.sh
+++ root/util/nvim.sh
@@ -1,4 +1,4 @@
-curl -o /tmp/nvim.tar.gz -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+curl -o /tmp/nvim-linux64.tar.gz -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 tar -xzvf /tmp/nvim-linux64.tar.gz
 mv /tmp/nvim-linux64/ $HOME/nvim/
 alias nvim='~/nvim/bin/nvim'
