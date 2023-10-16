e5a95b083b48273e354a5fa2d9b144eb3468a71a
Tobias Kendlbacher <kendlbat@edu.htl-villach.at>
2023-10-16 10:49:25 +0200

    Tar specify target dir

--- root/util/nvim.sh
+++ root/util/nvim.sh
@@ -1,5 +1,5 @@
 curl -o /tmp/nvim-linux64.tar.gz -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-tar -xzvf /tmp/nvim-linux64.tar.gz
+tar -xzvf /tmp/nvim-linux64.tar.gz -C /tmp/nvim-linux64
 mv /tmp/nvim-linux64/ $HOME/nvim/
 alias nvim='~/nvim/bin/nvim'
 echo alias nvim='~/nvim/bin/nvim' >> $HOME/.bashrc
