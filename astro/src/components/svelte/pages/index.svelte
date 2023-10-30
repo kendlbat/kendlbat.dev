d4c9396e2106e3137eb030f4161b1ea7debbc76b
Tobias Kendlbacher <kendlbat@edu.htl-villach.at>
2023-10-30 01:40:53 +0100

    New astro-based portfolio

---
+++ astro/src/components/svelte/pages/index.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+    import { Heading, P, Button, Card } from "flowbite-svelte";
+    import { ArrowRightOutline } from "flowbite-svelte-icons";
+    import Social from "./social.svelte";
+</script>
+
+<div>
+    <Heading tag="h2">Hi there!</Heading>
+    <P class="mb-6 mt-6 text-lg lg:text-xl">
+        I'm Tobias Kendlbacher and I'm a 17 year old student from Austria.
+    </P>
+    <Button href="/social">
+        My social media
+        <ArrowRightOutline class="animate-bounce-lr ml-2 h-3.5 w-3.5" />
+    </Button>
+</div>
+<div>
+    <Heading tag="h3" class="mb-6 mt-16 text-lg lg:text-xl">
+        Here are some of the things I am passionate about:
+    </Heading>
+    <div class="inline-block columns-1 sm:columns-2 md:columns-3 lg:columns-4">
+        <Card
+            class="mb-2 break-inside-avoid"
+            href="https://flybyguys.com/"
+            img="/assets/fbg.png">
+            <h5
+                class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Software Engineer at Fly-By Guys
+            </h5>
+        </Card>
+
+        <Card
+            href="https://verbotengut.at/"
+            img="/assets/acsc.png"
+            class="mb-2 break-inside-avoid">
+            <h5
+                class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                2<sup>nd</sup> place at the Austrian Cyber Security Challenge 2023
+            </h5>
+        </Card>
+
+        <Card
+            href="https://htl-villach.at/"
+            img="/assets/htl-villach.png"
+            class="mb-2 break-inside-avoid">
+            <h5
+                class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Computer Science Student at HTL Villach
+            </h5>
+        </Card>
+
+        <Card
+            href="https://theater-lichterloh.com/"
+            img="/assets/lichterloh.png"
+            class="mb-2 break-inside-avoid p-[0.5rem]">
+            <h5
+                class="m-[-0.5rem] mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Actor at Theater LicHTerLoh
+            </h5>
+        </Card>
+
+        <Card
+            href="https://first.global/"
+            img="/assets/fgc2023.png"
+            class="mb-2 max-w-[14rem] break-inside-avoid p-[0.5rem]">
+            <h5
+                class="m-[-0.5rem] mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                FIRST&reg; Global Challenge 2023
+            </h5>
+        </Card>
+    </div>
+</div>
---
+++ astro/src/components/svelte/pages/social.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+    import { Button, Tooltip } from "flowbite-svelte";
+    import { GithubSolid, LinkedinSolid } from "flowbite-svelte-icons";
+
+    export let outerClass: string = "";
+</script>
+
+<div class={outerClass}>
+    <div class="flex flex-row gap-3">
+        <!-- GitHub -->
+        <Button
+            class="bg-[#171515]"
+            href="https://github.com/kendlbat"
+            color="none">
+            <GithubSolid class="h-12 w-12 text-white" />
+        </Button>
+        <Tooltip>GitHub</Tooltip>
+
+        <!-- LinkedIn -->
+        <Button
+            class="bg-[#0077b5]"
+            href="https://www.linkedin.com/in/kendlbat"
+            color="none">
+            <LinkedinSolid class="h-12 w-12 text-white" />
+        </Button>
+        <Tooltip>LinkedIn</Tooltip>
+
+        <!-- Buy me a coffee -->
+        <Button
+            class="p-0"
+            href="https://www.buymeacoffee.com/kendlbat"
+            color="none">
+            <img
+                src="/assets/icons/buymeacoffee.png"
+                class="h-[4.2rem]"
+                alt="Buy me a coffee" />
+        </Button>
+        <Tooltip>Buy me a coffee</Tooltip>
+
+        <!-- Instagram -->
+        <Button
+            class="p-0"
+            href="https://www.instagram.com/kendlbat"
+            color="none">
+            <img
+                src="/assets/icons/instagram.png"
+                class="h-[4.2rem]"
+                alt="Instagram" />
+        </Button>
+        <Tooltip>Instagram</Tooltip>
+
+        <!-- Mastodon -->
+        <Button
+            class="p-0"
+            href="https://mastodon.social/@kendlbat"
+            color="none">
+            <img
+                src="/assets/icons/mastodon.svg"
+                class="h-[4.2rem]"
+                alt="Mastodon" />
+        </Button>
+        <Tooltip>Mastodon</Tooltip>
+    </div>
+</div>
