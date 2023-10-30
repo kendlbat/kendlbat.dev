703515fc3a7e6d8c66f29f21d77a145a3f1278e4
Tobias Kendlbacher <kendlbat@edu.htl-villach.at>
2023-10-30 15:04:00 +0100

    Sitemap & robots.txt

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import robotsTxt from "astro-robots-txt";
 import svelte from "@astrojs/svelte";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), svelte()],
+  site: "https://kendlbat.dev",
+  integrations: [tailwind(), svelte(), robotsTxt(), sitemap()]
 });
