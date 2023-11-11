63236a4ce3aef15484f7f448fd9d219bc17f02b7
Tobias Kendlbacher <kendlbat@edu.htl-villach.at>
2023-11-11 23:56:26 +0100

    Dark mode transition

--- astro/src/components/svelte/Nav.svelte
+++ astro/src/components/svelte/Nav.svelte
@@ -11,7 +11,7 @@
     export let activeUrl: string;
 </script>
 
-<Navbar class="bg-slate-200">
+<Navbar class="bg-slate-200 transition-colors duration-[200]">
     <NavBrand href="/">
         <span
             class="self-center whitespace-nowrap pl-3 text-xl font-semibold dark:text-white sm:pl-0">
